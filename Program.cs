f9884eeeb8fe14d313cc22599d182c1e6b4cedc8
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2023-02-19 17:29:23 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+
+namespace BotwNameConverter;
+
+internal static class Program
+{
+    private static IDictionary<string, string> _names = null!;
+
+    private static void Main()
+    {
+        WriteAsciiArt();
+        LoadNames();
+
+        // Check if the converted folder exists.
+        if(Directory.Exists("converted"))
+        {
+            // Clear the converted folder.
+            Directory.Delete("converted", true);
+        }
+            
+        // Create the new converter folder.
+        Directory.CreateDirectory("converted");
+
+        if (!Directory.Exists("files"))
+        {
+            Console.WriteLine("files directory does not exists. Press any key to close application...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        // Get all the files from the files folder and rename them.
+        foreach(var file in Directory.GetFiles("files"))
+        {
+            var fileName = Path.GetFileName(file);
+            var fileExt = Path.GetExtension(file);
+            var jsonKey = fileName.Replace(fileName, GetNameValue(fileName.Replace(".png", ""), fileName));
+            var newFileName = jsonKey + fileExt;
+                
+            File.Copy($"files/{fileName}", $"converted/{newFileName}", true);
+
+            Console.WriteLine($"Renamed {fileName} to {newFileName}");
+        }
+            
+        Console.WriteLine();
+        Console.WriteLine("BotwNameConverter is finished. Press any key to close application...");
+        Console.ReadKey();
+    }
+
+    private static void LoadNames()
+    {
+        // Check if the names.json file exists.
+        if (!File.Exists("names.json"))
+        {
+            Console.WriteLine("File (names.json) does not exists. Press any key to close application...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        // Store all json variables in the _names.
+        _names = JsonConvert.DeserializeObject<IDictionary<string, string>>(File.ReadAllText("names.json"))!;
+    }
+
+    private static void WriteAsciiArt()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine(@" ____        _              _   _                         _____                          _            ");
+        Console.WriteLine(@"|  _ \      | |            | \ | |                       / ____|                        | |           ");
+        Console.WriteLine(@"| |_) | ___ | |___      __ |  \| | __ _ _ __ ___   ___  | |     ___  _ ____   _____ _ __| |_ ___ _ __ ");
+        Console.WriteLine(@"|  _ < / _ \| __\ \ /\ / / | . ` |/ _` | '_ ` _ \ / _ \ | |    / _ \| '_ \ \ / / _ \ '__| __/ _ \ '__|");
+        Console.WriteLine(@"| |_) | (_) | |_ \ V  V /  | |\  | (_| | | | | | |  __/ | |___| (_) | | | \ V /  __/ |  | ||  __/ |   ");
+        Console.WriteLine(@"|____/ \___/ \__| \_/\_/   |_| \_|\__,_|_| |_| |_|\___|  \_____\___/|_| |_|\_/ \___|_|   \__\___|_|   ");
+        Console.ResetColor();
+        Console.WriteLine();
+        Console.WriteLine("BotwNameConverter version: v1.0");
+        Console.WriteLine("Created by Almighty-Shogun");
+        Console.WriteLine("GitHub: https://github.com/Almighty-Shogun/botw-name-converter");
+        Console.WriteLine();
+    }
+        
+    private static string GetNameValue(string key, string fallback = "") => _names.TryGetValue(key, out var value) ? value : fallback;
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# botw-name-converter
+<h1 align="center">
+<img src='https://cdn.shogunate.tools/assets/logo/png/Almighty_Shogun_Stars_Thick_Small.png'>
+<br>botw-name-converter</br>
+</h1>
+
 A simple script in C# that converts the icon names from The Legend of Zelda - Breath of the Wild to the actual item name.
+
+# Note
+This is an old project that I wrote a long time ago. I have converted the code from using .NET 5 to .NET 6 and published the code here on GitHub. I have no use for the code anymore and it might be useful so someone in the future. This is one of my first C# application that I wrote. If the code doesn't work or you need help with it feel free to contact me.
+
+# Information
+This is an C# console application that simply scans all the files in the `files` folder and renames the image name to the actual item name in the game. For example `Animal_Insect_A.png` will be renamed to `Hot-Footed Frog.png`.
+
+# How to use/build
+**How to use**
+- Download .NET 6 [here](//dotnet.microsoft.com/download/dotnet/6.0).
+- Download the latest release [here](//github.com/Almighty-Shogun/botw-name-converter/releases/latest).
+- Get all icon files and place them in the `files` folder.
+  - The icon files have to be a `.png` file.
+- Start the application.
+
+
+  **How to build**
+- Download .NET 6 [here](//dotnet.microsoft.com/download/dotnet/6.0).
+- Download/fork the code and open it with any C# IDE (Visual Studio or JetBrains Rider).
+- Get all icon files and place them in the `files` folder.
+    - The icon files have to be a `.png` file.
+- Check if `Newtonsoft.Json` version `13.0.1` or higher is installed (normally the IDE will do this automatically).
